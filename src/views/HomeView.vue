95b57813d71e1b985292f5cb8a5e0a2fe731c348
yemres28 <yunus.cakir@oneworkup.com>
2022-12-10 12:01:30 +0300

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <br />
+    <CustomText>Deneme</CustomText>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import CustomText from "@/components/CustomText.vue";
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    CustomText,
   },
 };
 </script>
